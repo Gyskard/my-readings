b02cd0a083fe4e1f8aea3d1f72edee808e5d7015
root <thomas@marguerit.at>
2020-05-05 22:15:07 +0200

    Add water.css, improve home, book and author view.

--- MyReadings/settings.py
+++ MyReadings/settings.py
@@ -122,6 +122,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = ()
 
 INTERNAL_IPS = [
     '10.0.2.2',
--- library/views.py
+++ library/views.py
@@ -1,16 +1,26 @@
-from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-from django.urls import reverse
 from django.views import generic
 
 from .models import Book, Author
 
+
 class IndexView(generic.ListView):
     template_name = 'library/index.html'
-    context_object_name = 'latest_book_list'
+    context_object_name = 'total_book_list'
 
     def get_queryset(self):
-        return Book.objects.all()[:10]
+        total_book_list = Book.objects.order_by('-has_been_read', 'title')
+        return total_book_list
+
+    def get_context_data(self, **kwargs):
+        number_total_book_list = len(Book.objects.order_by('-has_been_read', 'title'))
+        number_read_book_list = len(Book.objects.filter(has_been_read=True))
+
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['number_total_book_list'] = number_total_book_list
+        context['number_read_book_list'] = number_read_book_list
+        
+        return context
 
 class BookListView(generic.ListView):
     template_name = 'library/book_list.html'
@@ -19,9 +29,11 @@ class BookListView(generic.ListView):
     def get_queryset(self):
         return Book.objects.all()
 
+
 class BookDetailView(generic.DetailView):
     model = Book
     template_name = 'library/book_detail.html'
+
 
 class AuthorListView(generic.ListView):
     template_name = 'library/author_list.html'
@@ -30,7 +42,18 @@ class AuthorListView(generic.ListView):
     def get_queryset(self):
         return Author.objects.all()
 
+
 def AuthorDetailView(request, author_id):
     author = get_object_or_404(Author, pk=author_id)
-    book_list = Book.objects.filter(author=author_id)
-    return render(request, 'library/author_detail.html', {'author': author, 'book_list': book_list})
+    total_book_list = Book.objects.filter(author=author_id).order_by('-has_been_read', 'title')
+    number_total_book_list = len(total_book_list)
+    read_book_list = Book.objects.filter(author=author_id, has_been_read=True)
+    number_read_book_list = len(read_book_list)
+
+    return render(request, 'library/author_detail.html', {
+            'author': author,
+            'total_book_list': total_book_list,
+            'number_total_book_list': number_total_book_list,
+            'number_read_book_list': number_read_book_list
+        }
+    )
