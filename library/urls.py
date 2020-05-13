b8beeb1c1f603de40999a0c25c4d858f8c24a005
root <thomas@marguerit.at>
2020-05-13 13:24:45 +0200

    delete book list page and author list page due to redundancy with search page

--- library/urls.py
+++ library/urls.py
@@ -5,9 +5,7 @@ from . import views
 app_name = 'library'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('book/', views.BookListView.as_view(), name='book_list'),
     path('book/<int:pk>/', views.BookDetailView.as_view(), name='book_detail'),
-    path('author/', views.AuthorListView.as_view(), name='author_list'),
     path('author/<int:author_id>/', views.AuthorDetailView, name='author_detail'),
     path('search/', views.SearchView.as_view(), name='search'),
 ]
--- library/views.py
+++ library/views.py
@@ -21,42 +21,9 @@ class IndexView(generic.ListView):
         context['number_read_book_list'] = number_read_book_list
         return context
 
-class BookListView(generic.ListView):
-    template_name = 'library/book_list.html'
-    context_object_name = 'book_list'
-
-    def get_queryset(self):
-        return Book.objects.all().order_by('title')
-
-    def get_context_data(self, **kwargs):
-        number_total_book = len(Book.objects.order_by())
-        number_read_book = len(Book.objects.filter(has_been_read=True))
-        context = super(BookListView, self).get_context_data(**kwargs)
-        context['number_total_book'] = number_total_book
-        context['number_read_book'] = number_read_book
-        return context
-
-
 class BookDetailView(generic.DetailView):
     model = Book
     template_name = 'library/book_detail.html'
-
-
-class AuthorListView(generic.ListView):
-    template_name = 'library/author_list.html'
-    context_object_name = 'author_list'
-
-    def get_queryset(self):
-        #.annotate(number_total_book=Count('book__id'))
-        number_all_book = Count('book')
-        number_read_book = Count('book', filter=Q(book__has_been_read=True))
-        return Author.objects.all().annotate(number_all_book=number_all_book).annotate(number_read_book=number_read_book).order_by('name')
-
-    def get_context_data(self, **kwargs):
-        number_total_author = len(Author.objects.order_by())
-        context = super(AuthorListView, self).get_context_data(**kwargs)
-        context['number_total_author'] = number_total_author
-        return context
 
 def AuthorDetailView(request, author_id):
     author = get_object_or_404(Author, pk=author_id)
