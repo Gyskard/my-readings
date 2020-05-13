90a8f628d17ef9d220ec871cb24e2590b564ada5
root <thomas@marguerit.at>
2020-05-13 17:32:27 +0200

    refactor and pep8

--- library/urls.py
+++ library/urls.py
@@ -4,8 +4,23 @@ from . import views
 
 app_name = 'library'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('book/<int:pk>/', views.BookDetailView.as_view(), name='book_detail'),
-    path('author/<int:author_id>/', views.AuthorDetailView, name='author_detail'),
-    path('search/', views.SearchView.as_view(), name='search'),
+    path(
+        '',
+        views.IndexView.as_view(),
+        name='index'
+    ),
+    path(
+        'book/<int:pk>/',
+        views.BookDetailView.as_view(),
+        name='book_detail'
+    ),
+    path(
+        'author/<int:author_id>/',
+        views.AuthorDetailView,
+        name='author_detail'
+    ),
+    path(
+        'search/',
+        views.SearchView.as_view(),
+        name='search')
 ]
--- library/views.py
+++ library/views.py
@@ -4,48 +4,56 @@ from django.db.models import Count, Q
 
 from .models import Book, Author
 
+
 class IndexView(generic.ListView):
     template_name = 'library/index.html'
     context_object_name = 'total_book_list'
 
     def get_queryset(self):
-        total_book_list = Book.objects.order_by('id')[:10]
-        return total_book_list
+        book_list = Book.objects.order_by('id')[:10]
+        return book_list
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
-        context['number_total_book_list'] = len(Book.objects.order_by())
-        context['number_read_book_list'] = len(Book.objects.filter(has_been_read=True))
-        context['number_author'] = len(Author.objects.order_by())
+        authors = Author.objects.order_by()
+        total_books = Book.objects.order_by()
+        read_books = total_books.filter(has_been_read=True)
+        context['number_author'] = len(authors)
+        context['number_total_book_list'] = len(total_books)
+        context['number_read_book_list'] = len(read_books)
         return context
+
 
 class BookDetailView(generic.DetailView):
     model = Book
     template_name = 'library/book_detail.html'
 
+
 def AuthorDetailView(request, author_id):
-    author = get_object_or_404(Author, pk=author_id)
-    total_book_list = Book.objects.filter(author=author_id).order_by('-has_been_read', 'title')
-    number_total_book_list = len(total_book_list)
-    read_book_list = Book.objects.filter(author=author_id, has_been_read=True)
-    number_read_book_list = len(read_book_list)
+    books = Book.objects.filter(author=author_id)
+    total_book_list = books.order_by('-has_been_read', 'title')
+    read_book_list = books.filter(has_been_read=True)
     return render(request, 'library/author_detail.html', {
-            'author': author,
+            'author': get_object_or_404(Author, pk=author_id),
+            'number_read_book_list': len(read_book_list),
+            'number_total_book_list': len(total_book_list),
             'total_book_list': total_book_list,
-            'number_total_book_list': number_total_book_list,
-            'number_read_book_list': number_read_book_list
         }
     )
+
 
 class SearchView(generic.ListView):
     template_name = 'library/search.html'
     context_object_name = 'data'
 
     def get_queryset(self):
-        number_all_book = Count('book')
-        number_read_book = Count('book', filter=Q(book__has_been_read=True))
         context = {
             'books': Book.objects.all().order_by('title'),
-            'authors': Author.objects.all().annotate(number_all_book=number_all_book).annotate(number_read_book=number_read_book).order_by('name')
+            'authors': Author.objects.all()
+            .annotate(number_all_book=Count('book'))
+            .annotate(number_read_book=Count(
+                'book',
+                filter=Q(book__has_been_read=True)))
+            .order_by('name')
         }
         return context
