6719e370fce13b7cbd0c3f09207575d73139094c
root <thomas@marguerit.at>
2020-05-14 12:18:45 +0200

    add multiple settings file (base + developmenet)

---
+++ MyReadings/MyReadings/settings/development.py
@@ -0,0 +1,18 @@
+from .base import *
+
+DEBUG = True
+
+INSTALLED_APPS += [
+    'debug_toolbar',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+INTERNAL_IPS = [
+    '10.0.2.2',
+]
